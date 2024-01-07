30ac1a421c3a4f5f1bdf6caf4a8e9e6b80d9670a
Tony Rippy <tonyrippy@gmail.com>
2024-01-07 11:13:47 -0500

    Switch to forked version of Fraction.js that includes the precision fix.
    Fixes #6.

--- test/conversion.test.ts
+++ test/conversion.test.ts
@@ -3,6 +3,15 @@ import { Unit, UnitConverter, UNITS } from '../src/conversion'
 
 import Fraction from 'fraction.js'
 
+describe('Numeric limits', () => {
+  // TODO: Remove after https://github.com/rawify/Fraction.js/pull/74 is merged.
+  // Also switch dependency in package,json back to upstream.
+  test('1e12 doesn\'t overflow', () => {
+    let n = new Fraction(1e12)
+    expect(n.toString()).toBe('1000000000000')
+  })
+})
+
 describe('Data storage', () => {
   [
     [new Fraction(0), UNITS.get(ASTKinds.bit), new Fraction(0), UNITS.get(ASTKinds.byte)],
@@ -13,8 +22,8 @@ describe('Data storage', () => {
     [new Fraction(1), UNITS.get(ASTKinds.gibibyte), new Fraction(1_024), UNITS.get(ASTKinds.mebibyte)],
     [new Fraction(1), UNITS.get(ASTKinds.gibibyte), new Fraction(2**30), UNITS.get(ASTKinds.byte)],
     [new Fraction(1), UNITS.get(ASTKinds.gibibyte), new Fraction(2**30, 1e9), UNITS.get(ASTKinds.gigabyte)],
+    [new Fraction(1e-3), UNITS.get(ASTKinds.exabyte), new Fraction(1e12), UNITS.get(ASTKinds.kilobyte)],
   ].forEach(([fromQuantity, fromUnit, toQuantity, toUnit]) => {
-
     test(`${(fromQuantity as Fraction).toString()} ${(fromUnit as Unit).name} <--> ${(toQuantity as Fraction).toString()} ${(toUnit as Unit).name}`, () => {
       expect(fromUnit).not.toBeUndefined()
       expect(toUnit).not.toBeUndefined()
