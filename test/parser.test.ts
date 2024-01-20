059826080b820fa74a7e8a5d818481e64d81521e
Tony Rippy <tonyrippy@gmail.com>
2024-01-20 15:02:00 -0500

    Fixed some issues with unit abbreviations taking precedence over other rules.
    As part of this, I also:
    
     * Removed unnecessary subgroups from unit parsing rules.
     * Added negative lookaheads to prevent prefix matching.
     * Increased test coverage of unit parsing.

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -129,6 +129,180 @@ describe('Scientific number parsing', () => {
   })
 })
 
+function parseUnit(input: string): ASTKinds {
+  let ast = expectParse(`${input} per inch`) as ConversionQuery
+  return ast.toUnit.kind
+}
+
+describe('Data storage unit parsing', () => {
+  [
+    ['bit', ASTKinds.Bit],
+    ['bits', ASTKinds.Bit],
+    ['nibble', ASTKinds.Nibble],
+    ['nibbles', ASTKinds.Nibble],
+    ['b', ASTKinds.Byte],
+    ['byte', ASTKinds.Byte],
+    ['bytes', ASTKinds.Byte],
+    ['kB', ASTKinds.Kilobyte],
+    ['kilobyte', ASTKinds.Kilobyte],
+    ['kilobytes', ASTKinds.Kilobyte],
+    ['mb', ASTKinds.Megabyte],
+    ['MB', ASTKinds.Megabyte],
+    ['megabyte', ASTKinds.Megabyte],
+    ['megabytes', ASTKinds.Megabyte],
+    ['gb', ASTKinds.Gigabyte],
+    ['gigabyte', ASTKinds.Gigabyte],
+    ['gigabytes', ASTKinds.Gigabyte],
+    ['tb', ASTKinds.Terabyte],
+    ['TB', ASTKinds.Terabyte],
+    ['terabyte', ASTKinds.Terabyte],
+    ['terabytes', ASTKinds.Terabyte],
+    ['pb', ASTKinds.Petabyte],
+    ['petabyte', ASTKinds.Petabyte],
+    ['petabytes', ASTKinds.Petabyte],
+    ['eb', ASTKinds.Exabyte],
+    ['exabyte', ASTKinds.Exabyte],
+    ['exabytes', ASTKinds.Exabyte],
+    ['kib', ASTKinds.Kibibyte],
+    ['kibibyte', ASTKinds.Kibibyte],
+    ['kibibytes', ASTKinds.Kibibyte],
+    ['mib', ASTKinds.Mebibyte],
+    ['MiB', ASTKinds.Mebibyte],
+    ['mebibyte', ASTKinds.Mebibyte],
+    ['mebibytes', ASTKinds.Mebibyte],
+    ['gib', ASTKinds.Gibibyte],
+    ['GiB', ASTKinds.Gibibyte],
+    ['gibibyte', ASTKinds.Gibibyte],
+    ['gibibytes', ASTKinds.Gibibyte],
+    ['tib', ASTKinds.Tebibyte],
+    ['TiB', ASTKinds.Tebibyte],
+    ['tebibyte', ASTKinds.Tebibyte],
+    ['tebibytes', ASTKinds.Tebibyte],
+    ['pib', ASTKinds.Pebibyte],
+    ['pebibyte', ASTKinds.Pebibyte],
+    ['pebibytes', ASTKinds.Pebibyte],
+    ['eib', ASTKinds.Exbibyte],
+    ['exbibyte', ASTKinds.Exbibyte],
+    ['exbibytes', ASTKinds.Exbibyte],
+  ].forEach(([input, expected]) => {
+    test(input as string, () => {
+      expect(parseUnit(input as string)).toBe(expected)
+    })
+  })
+})
+
+describe('Time unit parsing', () => {
+  [
+    ['s', ASTKinds.Second],
+    ['sec', ASTKinds.Second],
+    ['secs', ASTKinds.Second],
+    ['second', ASTKinds.Second],
+    ['seconds', ASTKinds.Second],
+    ['ms', ASTKinds.Millisecond],
+    ['millisecond', ASTKinds.Millisecond],
+    ['milliseconds', ASTKinds.Millisecond],
+    ['μs', ASTKinds.Microsecond],
+    ['us', ASTKinds.Microsecond],
+    ['microsecond', ASTKinds.Microsecond],
+    ['microseconds', ASTKinds.Microsecond],
+    ['ns', ASTKinds.Nanosecond],
+    ['nanosecond', ASTKinds.Nanosecond],
+    ['nanoseconds', ASTKinds.Nanosecond],
+    ['min', ASTKinds.Minute],
+    ['mins', ASTKinds.Minute],
+    ['minute', ASTKinds.Minute],
+    ['minutes', ASTKinds.Minute],
+    ['hour', ASTKinds.Hour],
+    ['hours', ASTKinds.Hour],
+    ['day', ASTKinds.Day],
+    ['days', ASTKinds.Day],
+    ['week', ASTKinds.Week],
+    ['weeks', ASTKinds.Week],
+    ['month', ASTKinds.Month],
+    ['months', ASTKinds.Month],
+    ['year', ASTKinds.Year],
+    ['years', ASTKinds.Year],
+    ['leapyear', ASTKinds.LeapYear],
+    ['leap year', ASTKinds.LeapYear],
+    ['leap years', ASTKinds.LeapYear],
+    ['decade', ASTKinds.Decade],
+    ['decades', ASTKinds.Decade],
+    ['century', ASTKinds.Century],
+    ['centuries', ASTKinds.Century],
+    ['millennium', ASTKinds.Millennium],
+    ['millennia', ASTKinds.Millennium],
+    ['helek', ASTKinds.Helek],
+    ['chelek', ASTKinds.Helek],
+    ['חלק', ASTKinds.Helek],
+  ].forEach(([input, expected]) => {
+    test(input as string, () => {
+      expect(parseUnit(input as string)).toBe(expected)
+    })
+  })
+})
+
+describe('Length unit parsing', () => {
+  [
+    ['inch', ASTKinds.Inch],
+    ['inches', ASTKinds.Inch],
+    ['in', ASTKinds.Inch],
+    ['foot', ASTKinds.Foot],
+    ['feet', ASTKinds.Foot],
+    ['ft', ASTKinds.Foot],
+    ['yard', ASTKinds.Yard],
+    ['yards', ASTKinds.Yard],
+    ['yd', ASTKinds.Yard],
+    ['mi', ASTKinds.Mile],
+    ['mile', ASTKinds.Mile],
+    ['miles', ASTKinds.Mile],
+    ['meter', ASTKinds.Meter],
+    ['meters', ASTKinds.Meter],
+    ['metre', ASTKinds.Meter],
+    ['metres', ASTKinds.Meter],
+    ['m', ASTKinds.Meter],
+    ['cm', ASTKinds.Centimeter],
+    ['centimeter', ASTKinds.Centimeter],
+    ['centimeters', ASTKinds.Centimeter],
+    ['centimetre', ASTKinds.Centimeter],
+    ['centimetres', ASTKinds.Centimeter],
+    ['mm', ASTKinds.Millimeter],
+    ['millimeter', ASTKinds.Millimeter],
+    ['millimeters', ASTKinds.Millimeter],
+    ['millimetre', ASTKinds.Millimeter],
+    ['millimetres', ASTKinds.Millimeter],
+    ['nm', ASTKinds.Nanometer],
+    ['nanometer', ASTKinds.Nanometer],
+    ['nanometers', ASTKinds.Nanometer],
+    ['nanometre', ASTKinds.Nanometer],
+    ['nanometres', ASTKinds.Nanometer],
+    ['um', ASTKinds.Micrometer],
+    ['μm', ASTKinds.Micrometer],
+    ['micrometer', ASTKinds.Micrometer],
+    ['micrometers', ASTKinds.Micrometer],
+    ['micrometre', ASTKinds.Micrometer],
+    ['micrometres', ASTKinds.Micrometer],
+    ['dam', ASTKinds.Decameter],
+    ['decameter', ASTKinds.Decameter],
+    ['decameters', ASTKinds.Decameter],
+    ['decametre', ASTKinds.Decameter],
+    ['decametres', ASTKinds.Decameter],
+    ['hm', ASTKinds.Hectometer],
+    ['hectometer', ASTKinds.Hectometer],
+    ['hectometers', ASTKinds.Hectometer],
+    ['hectometre', ASTKinds.Hectometer],
+    ['hectometres', ASTKinds.Hectometer],
+    ['km', ASTKinds.Kilometer],
+    ['kilometer', ASTKinds.Kilometer],
+    ['kilometers', ASTKinds.Kilometer],
+    ['kilometre', ASTKinds.Kilometer],
+    ['kilometres', ASTKinds.Kilometer],
+  ].forEach(([input, expected]) => {
+    test(input as string, () => {
+      expect(parseUnit(input as string)).toBe(expected)
+    })
+  })
+})
+
 describe('Conversion parsing', () => {
 
   test('What is # X in Y?', () => {
@@ -149,8 +323,6 @@ describe('Conversion parsing', () => {
   })
 
   test('Y per X', () => {
-    // If the order of precedence is wrong, the parser will
-    // choose "y" from "yd" and assume it is years.
     let ast = expectParse('ft per yd') as ConversionQuery
     expect(ast.kind).toBe(ASTKinds.ConvertYtoX)
     expect(ast.quantity).toBeNull()
